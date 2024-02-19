c778e5a177fa4fbbc6d823faf9f229ad3f11938e
Andrew Jeffery <dev@jeffas.io>
2024-02-19 11:40:07 +0000

    Add check_properties function

--- src/checker.rs
+++ src/checker.rs
@@ -12,7 +12,7 @@ mod simulation;
 mod visitor;
 
 use crate::report::{ReportData, ReportDiscovery, Reporter};
-use crate::{Expectation, Fingerprint, Model};
+use crate::{Expectation, Fingerprint, Model, Property};
 use std::collections::{BTreeMap, HashMap};
 use std::fmt::{Debug, Display};
 use std::hash::Hash;
@@ -440,6 +440,27 @@ pub trait Checker<M: Model> {
             }
             Expectation::Sometimes => DiscoveryClassification::Example,
         }
+    }
+
+    /// A helper that checks examples exist for all `sometimes` properties and no counterexamples
+    /// exist for any `always`/`eventually` properties.
+    fn check_properties(&self) -> Vec<(Property<M>, bool)>
+    where
+        M::Action: Debug,
+        M::State: Debug,
+    {
+        self.model()
+            .properties()
+            .into_iter()
+            .map(|p| {
+                let success = match p.expectation {
+                    Expectation::Always => self.discovery(p.name).is_none(),
+                    Expectation::Eventually => self.discovery(p.name).is_none(),
+                    Expectation::Sometimes => self.discovery(p.name).is_some(),
+                };
+                (p, success)
+            })
+            .collect()
     }
 
     /// A helper that verifies examples exist for all `sometimes` properties and no counterexamples
